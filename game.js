01dd12a6d907c285d6c2a0b2379341be48702ef5
Maciej Galus <maciejgalus1988@gmail.com>
2020-11-22 19:48:02 +0100

    clickCard function

--- game.js
+++ game.js
@@ -4,8 +4,42 @@ const cardsColor = ['red', 'red', 'green', 'green', 'blue', 'blue', 'brown', 'br
 let cards = document.querySelectorAll('div');
 cards = [...cards];
 
+const startTime = new Date().getTime()
+let activeCard = '';
+let activeCards = [];
+
+const gamePairs = cards.length /2;
+let gameResult = 0;
+
 const clickCard = function(){
-console.log('klik');
+activeCard = this;
+
+activeCard.classList.remove('hidden');
+
+if (activeCards.length === 0){
+    console.log('1');
+    activeCards[0] = activeCard;
+    return
+} else {
+    console.log('2');
+    cards.forEach(card => card.removeEventListener('click', clickCard));
+        console.log(activeCards);
+        activeCards[1] = activeCard;
+setTimeout(function() {
+    if(activeCards[0].className === activeCards[1].className) {
+    console.log('wygrales')
+    activeCards.forEach(card => card.classList.add('off'));
+} 
+else {
+    activeCards.forEach(card => card.classList.add('hidden'));
+
+}}, 1000);
+        
+    
+}
+
+
+
 }
 
 const init = function(){
@@ -19,7 +53,11 @@ const init = function(){
         cards.forEach(function(card){
             card.classList.add('hidden');
             card.addEventListener('click', clickCard);
+            
         })
+
+        activeCard = '';
+        activeCards.length = 0;
     }, 2000);
 
 }
